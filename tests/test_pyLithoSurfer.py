e5bb03534ac43591487dce2d026ee69e45379efa
Romain Beucher <romain.beucher@anu.edu.au>
2020-08-26 21:52:08 +1000

    Update some tests

--- tests/test_pyLithoSurfer.py
+++ tests/test_pyLithoSurfer.py
@@ -1,5 +1,6 @@
 from pytest import fixture
 from pyLithoSurferAPI import Literature
+from pyLithoSurferAPI import SHRIMPDataPoint
 import pandas as pd
 
 #@fixture
@@ -41,68 +42,6 @@ def literature_example():
             'otherId': None}
 
 
-#@vcr.use_literature('tests/lithosurfer/literature-info.yml')
-def test_literature_info():
-    """ Test API call to get the literatures entries """
-
-    literature_instance = Literature(id=625501)
-    response = literature_instance.info()
-
-    assert isinstance(response, dict)
-    assert response['id'] == 625501, "The ID should be in response"
-    assert set(literature_keys()).issubset(list(response.keys())), "All keys should be in the response"
-
-
-def test_literature_get_all():
-    """ Test API Call to get all literatures entries """
-
-    data = Literature.get_all()
-    assert isinstance(data, pd.DataFrame)
-    assert set(literature_keys()).issubset(list(data.columns))
-    assert len(data) > 20
-
-def test_literature_create():
-    literature_instance = Literature(**literature_example())
-    response = literature_instance.push_new_entry()
-    assert isinstance(response, dict)
-    assert set(literature_keys()).issubset(list(response.keys())), "All keys should be in the response"
-
-def test_literature_to_json():
-    literature_instance = Literature(**literature_example())
-    value = literature_instance.to_json()
-
-def test_literature_to_dict():
-    literature_instance = Literature(**literature_example())
-    value = literature_instance.to_dict()
-    assert isinstance(value, dict)
-
-def test_literature_update():
-    literature_instance = Literature(**literature_example())
-    literature_instance.author = "Bob Dylan"
-    response = literature_instance.update_entry()
-    print(response)
-    assert isinstance(response, dict)
-    assert response["id"] == 625501
-    assert response["author"] == "Bob Dylan"
-
-def test_literature_create():
-    literature_instance = Literature(**literature_example())
-    response = literature_instance.push_new_entry()
-    assert isinstance(response, dict)
-    assert "id" in response.keys()
-    assert set(literature_keys()).issubset(list(response.keys())), "All keys should be in the response"
-
-def test_delete_entry():
-    literature_instance = Literature(**literature_example())
-    response = literature_instance.push_new_entry()
-    response = literature_instance.delete_entry()
-    assert isinstance(response, dict)
-
-
-### Test SHRIMP 
-
-from pyLithoSurferAPI import SHRIMPDataPoint
-
 def shrimp_keys():
     # Responsible only for returning the test data
     return ['id', 'ngrains', 'nspots', 'age', 'ageErrorMin', 'ageErrorMax',
@@ -127,16 +66,56 @@ def shrimp_example():
             'analyteName': None,
             'mountId': None}
 
+
+def test_literature_get_all():
+    """ Test API Call to get all literatures entries """
+
+    data = Literature.get_all()
+    assert isinstance(data, pd.DataFrame)
+    assert set(literature_keys()).issubset(list(data.columns))
+    assert len(data) > 20
+
+def test_literature_create():
+    literature_instance = Literature(**literature_example())
+    response = literature_instance.new()
+    assert isinstance(response, dict)
+    assert set(literature_keys()).issubset(list(response.keys())), "All keys should be in the response"
+
+def test_literature_to_json():
+    literature_instance = Literature(**literature_example())
+    value = literature_instance.to_json()
+
+def test_literature_to_dict():
+    literature_instance = Literature(**literature_example())
+    value = literature_instance.to_dict()
+    assert isinstance(value, dict)
+
+def test_literature_update():
+    literature_instance = Literature(**literature_example())
+    literature_instance.author = "Bob Dylan"
+    response = literature_instance.update()
+    assert isinstance(response, dict)
+    assert response["id"] == 625501
+    assert response["author"] == "Bob Dylan"
+
+def test_literature_create():
+    literature_instance = Literature(**literature_example())
+    response = literature_instance.new()
+    assert isinstance(response, dict)
+    assert "id" in response.keys()
+    assert set(literature_keys()).issubset(list(response.keys())), "All keys should be in the response"
+
+def test_delete_entry():
+    literature_instance = Literature(**literature_example())
+    response = literature_instance.new()
+    response = literature_instance.delete()
+    assert isinstance(response, dict)
+
+### Test SHRIMP 
+
 def test_shrimp_data_get_all():
     data = SHRIMPDataPoint.get_all()
     assert isinstance(data, pd.DataFrame)
-
-def test_shrimp_info():
-    shrimp_instance = SHRIMPDataPoint(id=638451)
-    response = shrimp_instance.info()
-    assert isinstance(response, dict)
-    assert response['id'] == 638451, "The ID should be in response"
-    assert set(shrimp_keys()).issubset(list(response.keys())), "All keys should be in the response"
 
 def test_shrimp_data_id():
     shrimp_instance = SHRIMPDataPoint()
@@ -145,7 +124,7 @@ def test_shrimp_data_id():
 
 def test_shrimp_data_create():
     shrimp_instance = SHRIMPDataPoint(**shrimp_example())
-    response = shrimp_instance.push_new_entry()
+    response = shrimp_instance.new()
     assert isinstance(response, dict)
     assert "id" in response.keys()
     assert set(shrimp_keys()).issubset(list(response.keys())), "All keys should be in the response"
@@ -153,7 +132,7 @@ def test_shrimp_data_create():
 def test_shrimp_data_update():
     shrimp_instance = SHRIMPDataPoint(**shrimp_example())
     shrimp_instance.ngrains = 20
-    response = shrimp_instance.update_entry()
+    response = shrimp_instance.update()
     print(shrimp_instance.id)
     assert isinstance(response, dict)
     assert response["id"] == 638451
@@ -161,6 +140,6 @@ def test_shrimp_data_update():
     
 def test_shrimp_data_delete_entry():
     shrimp_instance = SHRIMPDataPoint(**shrimp_example())
-    response = shrimp_instance.push_new_entry()
-    response = shrimp_instance.delete_entry()
+    response = shrimp_instance.new()
+    response = shrimp_instance.delete()
     assert isinstance(response, dict)
