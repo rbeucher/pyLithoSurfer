0329ee6a9dfdd8b04e7a55fcbccd2e11aaf42deb
Romain Beucher <romain.beucher@anu.edu.au>
2020-09-21 21:15:15 +1000

    Update location

--- pyLithoSurferAPI/location.py
+++ pyLithoSurferAPI/location.py
@@ -36,12 +36,52 @@ class Location(APIRequests):
         self._lat = value
 
     @property
+    def locCaptureId(self):
+        return self._locCaptureId
+
+    @locCaptureId.setter
+    def locCaptureId(self, value):
+        self._locCaptureId = value
+
+    @property
+    def locCaptureMethod(self):
+        return self._locCaptureMethod
+
+    @locCaptureMethod.setter
+    def locCaptureMethod(self, value):
+        self._locCaptureMethod = value
+
+    @property
     def name(self):
         return self._name
 
     @name.setter
     def name(self, value):
         self._name = value
+
+    @property
+    def locPredefinedId(self):
+        return self._locPredefinedId
+
+    @locPredefinedId.setter
+    def locPredefinedId(self, value):
+        self._locPredefinedId = value
+
+    @property
+    def locPredefinedName(self):
+        return self._locPredefinedName
+
+    @locPredefinedName.setter
+    def locPredefinedName(self, value):
+        self._locPredefinedName = value
+
+    @property
+    def locKindId(self):
+        return self._locKindId
+
+    @locKindId.setter
+    def locKindId(self, value):
+        self._locKindId = value
 
     @property
     def lon(self):
@@ -58,11 +98,3 @@ class Location(APIRequests):
     @latLonPrecision.setter
     def latLonPrecision(self, value):
         self._latLonPrecision = value
-
-    @property
-    def celestial(self):
-        return self._celestial
-
-    @celestial.setter
-    def celestial(self, value):
-        self._celestial = value
