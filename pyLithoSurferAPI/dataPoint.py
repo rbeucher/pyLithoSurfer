af5a6bdba9aa4419d9f9f5ba3eed2d4f1a48e000
Romain Beucher <romain.beucher@anu.edu.au>
2020-10-03 16:47:49 +1000

    Update DataPoint class

--- pyLithoSurferAPI/dataPoint.py
+++ pyLithoSurferAPI/dataPoint.py
@@ -44,36 +44,12 @@ class DataPoint(APIRequests):
         self._dataStructure = value
 
     @property
-    def elevation(self):
-        return self._elevation
-
-    @elevation.setter
-    def elevation(self, value):
-        self._elevation = value
-
-    @property
     def id(self):
         return self._id
 
     @id.setter
     def id(self, value):
         self._id = value
-
-    @property
-    def latitude(self):
-        return self._latitude
-
-    @latitude.setter
-    def latitude(self, value):
-        self._latitude = value
-
-    @property
-    def longitude(self):
-        return self._longitude
-
-    @longitude.setter
-    def longitude(self, value):
-        self._longitude = value
 
     @property
     def name(self):
@@ -83,3 +59,58 @@ class DataPoint(APIRequests):
     def name(self, value):
         self._name = value
 
+    @property
+    def sampleId(self):
+        return self._sampleId
+
+    @sampleId.setter
+    def sampleId(self, value):
+        self._sampleId = value
+    
+    @property
+    def sampleName(self):
+        return self._sampleName
+
+    @sampleName.setter
+    def sampleName(self, value):
+        self._sampleName = value
+    
+    @property
+    def sourceId(self):
+        return self._sourceId
+
+    @sourceId.setter
+    def sourceId(self, value):
+        self._sourceId = value
+    
+    @property
+    def locationId(self):
+        return self._locationId
+
+    @locationId.setter
+    def locationId(self, value):
+        self._locationId = value
+    
+    @property
+    def locationName(self):
+        return self._locationName
+
+    @locationName.setter
+    def locationName(self, value):
+        self._locationName = value
+    
+    @property
+    def externalDataHref(self):
+        return self._externalDataHref
+
+    @externalDataHref.setter
+    def externalDataHref(self, value):
+        self._externalDataHref = value
+    
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        self._description = value
