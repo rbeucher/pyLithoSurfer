7c0126258c8f88f17624db367c0e91c2e5b27e27
Romain Beucher <romain.beucher@anu.edu.au>
2022-02-04 16:49:38 +1100

    Helium Model

---
+++ pyLithoSurferAPI/UThHe/lists.py
@@ -0,0 +1,61 @@
+
+from pyLithoSurferAPI.REST import APIRequests
+
+class LHeAgeEquation(APIRequests):
+
+    API_PATH = "/api/helium/LHeAgeEquation"
+
+
+class LHeAlphaStopDistRef(APIRequests):
+
+    API_PATH = "/api/helium/LHeAlphaStopDistRef"
+
+
+class LHeCorrectedAgeMethod(APIRequests):
+
+    API_PATH = "/api/helium/LHeCorrectedAgeMethod"
+
+
+class LHeCrysFrag(APIRequests):
+
+    API_PATH = "/api/helium/LHeCrysFrag"
+
+
+class LHeeUEquation(APIRequests):
+
+    API_PATH = "/api/helium/LHeeUEquation"
+
+
+class LHeFTEquation(APIRequests):
+
+    API_PATH = "/api/helium/LHeFTEquation"
+
+
+class LHeGrainDimensionEq(APIRequests):
+
+    API_PATH = "/api/helium/LHeGrainDimensionEq"
+
+
+class LHeGrainMorphology(APIRequests):
+
+    API_PATH = "/api/helium/LHeGrainMorphology"
+
+
+class LHeGrainGeometry(APIRequests):
+
+    API_PATH = "/api/helium/LHeGrainGeometry"
+
+
+class LHeRFTEq(APIRequests):
+
+    API_PATH = "/api/helium/LHeRFTEq"
+
+
+class LHeRSVEq(APIRequests):
+
+    API_PATH = "/api/helium/LHeRSVEq"
+
+
+class LLambda(APIRequests):
+
+    API_PATH = "/api/helium/LLambda"
---
+++ pyLithoSurferAPI/UThHe/upload.py
@@ -0,0 +1,203 @@
+from pyLithoSurferAPI.UThHe.schemas import HeDataPointSchema, HeWholeGrainSchema
+from pyLithoSurferAPI.core.lists import LErrorType, ReferenceMaterial
+
+from pyLithoSurferAPI.core.tables import DataPoint, Material
+from pyLithoSurferAPI.UThHe.tables import HeDataPoint, HeDataPointCRUD, HeWholeGrain
+from pyLithoSurferAPI.UThHe.lists import (LHeAgeEquation,
+                                          LHeAlphaStopDistRef,
+                                          LHeCorrectedAgeMethod,
+                                          LHeCrysFrag,
+                                          LHeeUEquation,
+                                          LHeFTEquation,
+                                          LHeGrainDimensionEq,
+                                          LHeGrainGeometry,
+                                          LHeGrainMorphology,
+                                          LHeRFTEq,
+                                          LHeRSVEq,
+                                          LLambda)
+
+from pyLithoSurferAPI.uploader import Uploader
+from pyLithoSurferAPI.management.tables import DataPackage
+from tqdm import tqdm
+
+
+
+class HeDataPointUploader(Uploader):
+
+    name = "HeDataPoints"
+
+    def __init__(self, datapackageId, he_datapoints_df):
+
+        self.datapackageId = datapackageId 
+        self.dataframe = he_datapoints_df
+        self.validated = False
+
+    def validate(self):
+
+        he_list = {"dataPackage": DataPackage,
+                   "alphaStopDistRef": LHeAlphaStopDistRef,
+                   "correctedHeAgeMethod": LHeCorrectedAgeMethod,
+                   "euequation": LHeeUEquation,
+                   "ftEquation": LHeFTEquation,
+                   "grainDimensionEquation": LHeGrainDimensionEq,
+                   "heAgeEquation": LHeAgeEquation,
+                   "meanCorrectedHeAgeError": LErrorType,
+                   "meanUncorrectedHeAgeErrorType": LErrorType,
+                   "mineral": Material,
+                   "referenceMaterial": ReferenceMaterial,
+                   "rftequation": LHeRFTEq,
+                   "rsvequation": LHeRSVEq,
+                   "weightedMeanCorrectedHeAgeErrorType": LErrorType,
+                   "weightedMeanUncorrectedHeAgeErrorType": LErrorType
+                   }
+
+        self.dataframe = Uploader._validate(self.dataframe, HeDataPointSchema, he_list)
+        self.validated = True
+
+    def upload(self, update=False, update_strategy="replace"):
+        
+        if not self.validated:
+            raise ValueError("Data not validated")
+
+        self.dataframe["id"] = None
+
+        for index in tqdm(self.dataframe.index):
+
+            he_args = self.dataframe.loc[index].to_dict()
+            sampleId = he_args.pop("sampleId")
+            locationId = he_args.pop("locationId")
+            if he_args.get("dataPointId"):
+                he_args.pop("dataPointId")
+
+            dpts_args = {"dataPackageId": self.datapackageId,
+                         "dataStructure": "HE",
+                         "dataEntityId": None,
+                         "name": None,
+                         "locationId": locationId,
+                         "sampleId": sampleId}
+            
+            query = {"dataPointLithoCriteria.dataStructure.equals": "HE",
+                     "dataPointLithoCriteria.sampleId.equals": int(sampleId),
+                     "dataPointLithoCriteria.dataPackageId.equals": self.datapackageId}
+
+            response = HeDataPointCRUD.query(query)
+            records = response.json()
+
+            if len(records) == 1:
+                existing_id = records[0]["id"]
+            elif len(records) > 1:
+                existing_id = records[0]["id"]
+            else:
+                existing_id = None
+
+            if existing_id is None:
+
+                # Create DataPoint
+                datapoint = DataPoint(**dpts_args)
+
+                # Create FTDataPoint
+                he_datapoint = HeDataPoint(**he_args)
+
+                # Use FTDataPointCRUD to create the Datapoint and
+                # the FTDatapoint
+                HeDataptsCRUD = HeDataPointCRUD(datapoint, he_datapoint) 
+                HeDataptsCRUD.new() 
+                
+                # Recover Datapoint
+                self.dataframe.loc[index, "id"] = HeDataptsCRUD.id
+                self.dataframe.loc[index, "dataPointId"] = HeDataptsCRUD.dataPoint.id
+
+            elif update:
+
+                old_dpts_args = records[0]["dataPointDTO"]
+                dpts_args = self._update_args(old_dpts_args, dpts_args, update_strategy)
+                old_he_args = records[0]["ftdataPointDTO"]
+                he_args = self._update_args(old_he_args, he_args, update_strategy)
+
+                # Create DataPoint
+                datapoint = DataPoint(**dpts_args)
+
+                # Create FTDataPoint
+                he_datapoint = HeDataPoint(**he_args)
+
+                # Use FTDataPointCRUD to create the Datapoint and
+                # the FTDatapoint
+                HeDataptsCRUD = HeDataPointCRUD(datapoint, he_datapoint) 
+                HeDataptsCRUD.id = he_datapoint.id
+                HeDataptsCRUD.dataPointID = datapoint.id
+                HeDataptsCRUD.dataPoint.dataEntityId = he_datapoint.id
+                HeDataptsCRUD.dataPoint.ftdatapoint_id = he_datapoint.id
+                HeDataptsCRUD.update()
+                self.dataframe.loc[index, "id"] = HeDataptsCRUD.id
+                self.dataframe.loc[index, "dataPointId"] = datapoint.id
+
+
+class HeWholeGrainsUploader(HeWholeGrain, Uploader):
+
+    name = "HeWholeGrain"
+
+    def __init__(self, datapackageId, he_whole_grains_df):
+
+        self.datapackageId = datapackageId 
+        self.dataframe = he_whole_grains_df
+        self.validated = False
+
+    def validate(self):
+
+        he_list = {"dataPackage": DataPackage,
+                   "aliquotMassErrorType": LErrorType,
+                   #"aliquotType": None,
+                   "crysFrag": LHeCrysFrag,
+                   "euerrorType": LErrorType,
+                   "ftErrorType": LErrorType,
+                   "grainGeometry": LHeGrainGeometry,
+                   "grainMorphology": LHeGrainMorphology,
+                   "he4AmountErrorType": LErrorType,
+                   "he4ConcentrationErrorType": LErrorType,
+                   "smAmountErrorType": LErrorType,
+                   "smConcentrationErrorType": LErrorType,
+                   "tauErrorType": LErrorType,
+                   "tauFTErrorType": LErrorType,
+                   "thAmountErrorType": LErrorType,
+                   "thConcentrationErrorType": LErrorType,
+                   "uamountErrorType": LErrorType,
+                   "uconcentrationErrorType": LErrorType,
+                   "uncorrectedHeAgeErrorTyp": LErrorType,
+                   "zrContentErrorType": LErrorType
+                   }
+
+        self.dataframe = Uploader._validate(self.dataframe, HeWholeGrainSchema, he_list)
+        self.validated = True
+
+    def get_unique_query(self, args):
+        
+        query = {"hedataPointId.equals": args["hedataPointId"],
+                 "aliquotID.equals": args["aliquotID"]}
+        return super().query(query)
+    
+    def upload(self, update=False, update_strategy="merge_keep"):
+        
+        self.dataframe["id"] = None
+
+        for index in tqdm(self.dataframe.index):
+
+            args = self.dataframe.loc[index].to_dict()
+            response = self.get_unique_query(args)
+            records = response.json()
+
+            if len(records) == 1:
+                existing_id = records[0]["id"]
+                old_args =  {k:v for k,v in records[0].items() if v is not None}
+            else:
+                existing_id = None
+
+            if existing_id is None:
+                obj = HeWholeGrain(**args) 
+                obj.new() 
+
+            elif update:
+                args = self._update_args(old_args, args, update_strategy)
+                obj = HeWholeGrain(**args) 
+                obj.update()
+
+            self.dataframe.loc[index, "id"] = obj.id
