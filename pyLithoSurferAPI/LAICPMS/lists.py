89150915bd407c063333ef8dbba6c536f97d3a67
Romain Beucher <romain.beucher@anu.edu.au>
2022-06-03 18:10:16 +1000

    LAICPMS Model

---
+++ pyLithoSurferAPI/LAICPMS/lists.py
@@ -0,0 +1,58 @@
+from pyLithoSurferAPI.REST import APIRequests
+
+
+class LCombinedMeasurement(APIRequests):
+
+    API_PATH = "/api/laicpms/LCombinedMeasurement"
+
+
+class LDataReductionPackage(APIRequests):
+
+    API_PATH = "/api/laicpms/LDataReductionPackage"
+
+
+class LInternalStandard(APIRequests):
+
+    API_PATH = "/api/laicpms/LInternalStandard"
+
+
+class LLaserPulseWidthUnit(APIRequests):
+
+    API_PATH = "/api/laicpms/LLaserPulseWidthUnit"
+
+
+class LLaserSamplingMode(APIRequests):
+
+    API_PATH = "/api/laicpms/LLaserSamplingMode"
+
+
+class LLaserSystem(APIRequests):
+
+    API_PATH = "/api/laicpms/LLaserSystem"
+
+
+class LLaserWaveLength(APIRequests):
+
+    API_PATH = "/api/laicpms/LLaserWaveLength"
+
+
+class LSampleFormat(APIRequests):
+
+    API_PATH = "/api/laicpms/LSampleFormat"
+
+
+class LTexture(APIRequests):
+
+    API_PATH = "/api/laicpms/LTexture"
+
+class LICPMSModel(APIRequests):
+
+    API_PATH = "/api/laicpms/LICPMSModel"
+
+
+class LICPMSType(APIRequests):
+
+    API_PATH = "/api/laicpms/LICPMSType"
+
+
+
